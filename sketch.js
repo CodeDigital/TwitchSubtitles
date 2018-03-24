765b97647068ef753a8effdc8f2a251b29e783f4
DigitalData <legoxavierlocketravers@gmail.com>
2018-03-24 18:45:54 +1100

    Initial Commit

---
+++ sketch.js
@@ -0,0 +1,31 @@
+p5.disableFriendlyErrors = true;
+
+var foo; // speech recognition object (will prompt for mic access)
+
+function preload(){
+
+}
+
+function setup() {
+  noCanvas();
+  var lang = navigator.language || 'en-US';
+
+  function showResult(){
+    var test = document.getElementById("stt");
+    var txt = test.innerHTML;
+    //txt = foo.resultString + "<br>" + txt;
+    txt = foo.resultString;
+    test.innerHTML = txt;
+    //createP(foo.resultString);
+    print(foo.resultString); // log the result
+  }
+
+  foo = new p5.SpeechRec(lang,showResult);
+var continuous = true; // boolean to set whether the speech recognition engine will give results continuously (true) or just once (false = default). bind callback function to trigger when speech is recognized
+var interim = true;
+foo.start(continuous, interim); // start listening
+}
+
+function draw() {
+
+}
