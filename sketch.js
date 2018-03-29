4ebf91cbefedd947c37b15ba076944f853edbd85
DigitalData <legoxavierlocketravers@gmail.com>
2018-03-29 16:41:57 +1100

    interim

--- sketch.js
+++ sketch.js
@@ -22,7 +22,7 @@ function setup() {
 
   foo = new p5.SpeechRec(lang,showResult);
 var continuous = true; // boolean to set whether the speech recognition engine will give results continuously (true) or just once (false = default). bind callback function to trigger when speech is recognized
-var interim = false;
+var interim = true;
 foo.start(continuous, interim); // start listening
 }
 
